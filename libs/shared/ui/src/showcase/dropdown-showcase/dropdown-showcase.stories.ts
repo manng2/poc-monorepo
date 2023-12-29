473aa13e1c25e1644f8e721964e33949e4bac3c3
Mike Nguyen <mike@rbtsb.com>
2023-12-29 17:03:39 +0800

    feat: dropdown selection

--- libs/shared/ui/src/showcase/dropdown-showcase/dropdown-showcase.stories.ts
+++ libs/shared/ui/src/showcase/dropdown-showcase/dropdown-showcase.stories.ts
@@ -1,6 +1,7 @@
 import { StoryObj, type Meta } from '@storybook/angular';
 import { DropdownShowcaseComponent } from './dropdown-showcase.component';
 import { within } from '@storybook/testing-library';
+import { wait } from '@people/shared-utils';
 
 const meta: Meta<DropdownShowcaseComponent> = {
   component: DropdownShowcaseComponent,
@@ -16,13 +17,17 @@ export const Behavior: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
 
+    await wait(.5);
+
     const dropdown = await canvas.findByTestId('dropdown-button');
 
-    await dropdown.click();
+    dropdown.click();
+
+    await wait(1);
 
     const option = await canvas.findAllByTestId('dropdown-option');
 
-    await option[0].click();
+    option[0].click();
   }
 };
 
--- libs/tailwind-preset/tailwind.config.js
+++ libs/tailwind-preset/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     colors: {
       primary: '#540eff',
       'light-gray': '#E4E4E4',
+      white: '#FFFFFF',
+      blue: {
+        100: '#4091E1',
+        200: '#0056AC',
+        300: '#1E2778',
+        400: '#0602f1',
+      },
     },
     borderWidth: {
       '1': '1px'
